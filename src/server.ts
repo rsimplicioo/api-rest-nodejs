b97ad28f95f79a93ed70c1ecd50e9283465021c0
Rafael Simplicio <simplicio.rsb@gmail.com>
2023-09-11 15:26:12 -0300

    feat: adicionando variaveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import fastify from 'fastify'
-import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = knex('sqlite_schema').select('*')
-  return tables
+  return 'transactions'
 })
 
 app
