c313fee39990302494c6d5986f18a6c1712a047b
Rafael Simplicio <simplicio.rsb@gmail.com>
2023-09-11 23:57:28 -0300

    feat: adicionando `middleware` para verificacao dos sessionId dos cookies

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
+
+app.register(cookie)
+
+app.addHook('preHandler', async (request) => {
+  console.log(`[${request.method} ${request.url}]`)
+})
 
 app.register(transactionsRoutes, {
   prefix: 'transactions',
